f0aca13ddb862900cab982f5e0c04785d990c14f
1903004 <1903004@1903004NB01.iead.local>
2019-11-08 16:24:26 +0800

    FindCommomCharacters

--- src/main/java/org/hugh/leetcode/Algorithms/FindCommonCharacters.java
+++ src/main/java/org/hugh/leetcode/Algorithms/FindCommonCharacters.java
@@ -20,13 +20,17 @@ public class FindCommonCharacters {
 	 * letter
 	 */
 	public static void main(String[] args) {
-		String[] aStrings = { "acabcddd", "bcbdbcbd", "baddbadb", "cbdddcac", "aacbcccd", "ccccddda", "cababaab",
-				"addcaccd" };
-//		String[] aStrings = { "bella", "label", "roller" };
+//		String[] aStrings = { "acabcddd", "bcbdbcbd", "baddbadb", "cbdddcac", "aacbcccd", "ccccddda", "cababaab",
+//				"addcaccd" };
+		String[] aStrings = { "bella", "label", "roller" };
 //		String[] aStrings = {"cool", "lock", "cook"};
 		System.out.println(findCommonCharacters(aStrings));
 	}
-
+	
+	/**
+	 * Runtime: 12 ms, faster than 29.86% of Java online submissions for Find Common Characters.
+	 * Memory Usage: 38.4 MB, less than 62.07% of Java online submissions for Find Common Characters.
+	 */
 	public static List<String> findCommonCharacters(String[] A) {
 		List<String> chList = new ArrayList<>();
 		int index = 0;
@@ -37,42 +41,41 @@ public class FindCommonCharacters {
 				index = i;
 			}
 		}
-		char[] arrayJ = A[index].toCharArray();
-		for (char ch : arrayJ) {
+		char[] shortestCharArr = A[index].toCharArray();/* 從長度最短的字串當基準開始查 */
+		for (char ch : shortestCharArr) {
 			int count = 0;
-			boolean notFound = true;
+			boolean isFound = false;/* 這個字元有沒有在這個字串被找到 */
 			for (int i = 0; i < A.length; i++) {
-				int thiscount = count;
-				char[] arrayK = A[i].toCharArray();
-				for (char ch1 : arrayK) {
+				int thiscount = 0;
+				char[] thisStrArr = A[i].toCharArray();
+				for (char ch1 : thisStrArr) {
 					if (ch == ch1) {
+						thiscount++;
 						count++;
-						notFound = false;
+						isFound = true;
 					}
 				}
-				if(thiscount == count) {
-					notFound =true;
+				if (thiscount == 0) {
+					isFound = false;
 					break;
+				} else {
+					List<String> chList1 = chList;
+					int checkCount = 0;
+					for (String str : chList1) {
+						if (str.equals(String.valueOf(ch))) {
+							checkCount++;
+						}
+					}
+					if (thiscount > checkCount) {
+						isFound = true;
+					}else {
+						isFound = false;
+						break;
+					}
 				}
 			}
-			if (!notFound) {
-				if (count != 0 && (count % A.length == 0 || (count / A.length) >= 1)) {
-					List<String> chList1 = chList;
-					if ((count / A.length) >= 1) {
-						int countChar = (count / A.length);
-						int checkCount = 0;
-						for (String str : chList1) {
-							if (str.equals(String.valueOf(ch))) {
-								checkCount++;
-							}
-						}
-						if (countChar > checkCount) {
-							chList.add(String.valueOf(ch));
-						}
-					} else {
-						chList.add(String.valueOf(ch));
-					}
-				}
+			if (isFound) {
+				chList.add(String.valueOf(ch));
 			}
 		}
 		return chList;

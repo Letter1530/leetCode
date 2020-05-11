a02b38e617ff15f86d74e5f95f0e136bc77993c0
Bug製造機 <deadfantasy612@gmail.com>
2020-05-11 17:21:05 +0800

    LC_0014 complete

--- src/main/java/org/engine/Algorithms/LC_0014_LongestCommonPrefix.java
+++ src/main/java/org/engine/Algorithms/LC_0014_LongestCommonPrefix.java
@@ -1,31 +1,57 @@
 package org.engine.Algorithms;
 
-public class LC_0014_LongestCommonPrefix {
-	/*
-	 * Write a function to find the longest common prefix string amongst an array of strings.
-	 * If there is no common prefix, return an empty string "".
-	 * 
-	 * Example 1:
-	 * Input: ["flower","flow","flight"]
-	 * Output: "fl"
-	 * 
-	 * Example 2:
-	 * Input: ["dog","racecar","car"]
-	 * Output: ""
-	 * Explanation: There is no common prefix among the input strings.
-	 * */
-	
-	public static void main(String[] args) {
-		String[] strs = {"aca","cba"};
-		System.out.println(longestCommonPrefix(strs));
-	}
+import lombok.extern.slf4j.Slf4j;
 
-	/*
-	 * Runtime: ?? ms, faster than ??% of Java online submissions for Longest Common Prefix.
-	 * Memory Usage: ?? MB, less than ??% of Java online submissions
-	 * for Longest Common Prefix.
-	 */
-	public static String longestCommonPrefix(String[] strs) {
-		return null;
-	}
+@Slf4j
+public class LC_0014_LongestCommonPrefix {
+
+    /**
+     *
+     *  Write a function to find the longest common prefix string amongst an array of strings.
+     *  If there is no common prefix, return an empty string "".
+     *
+     *  Example 1:
+     *  Input: ["flower","flow","flight"]
+     *  Output: "fl"
+     *
+     *  Example 2:
+     *  Input: ["dog","racecar","car"]
+     *  Output: ""
+     *  Explanation: There is no common prefix among the input strings.
+     *
+     * @Author BugDeveloper
+     * @Description
+     * @Date 下午 05:19 2020/5/11
+     * @Param [args]
+     * @return void
+     **/
+    public static void main(String[] args) {
+//        String[] strs = {"flower", "flow", "flight"};
+//        String[] strs = {"aca", "cba"};
+//        String[] strs = ["dog", "racecar", "car"];
+        String[] strs = {"bcdef", "bcde", "bc"};
+        log.info("ans:[{}]", longestCommonPrefix(strs));
+    }
+
+    /*
+     * Runtime: 0 ms, faster than 100% of Java online submissions for Longest Common Prefix.
+     * Memory Usage: 37.5 MB, less than 91.23% of Java online submissions
+     * for Longest Common Prefix.
+     */
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0)
+            return "";
+
+        String res = strs[0];
+        int i = 1;
+        while (i < strs.length) {
+            if (strs[i].indexOf(res) != 0) {
+                res = res.substring(0, res.length() - 1);
+            } else {
+                i++;
+            }
+        }
+        return res;
+    }
 }
